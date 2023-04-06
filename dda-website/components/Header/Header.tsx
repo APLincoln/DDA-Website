d1c8934a8aa03d9937421252958bb0a907360efe
Arron <UP2044509@myport.ac.uk>
2023-04-06 20:21:54 +0100

    feat-adding some folders

---
+++ dda-website/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="w-full h-10" >
+      <div>This is a header component</div>
+    </div>
+  )
+}
+
+export default Header
