7ab3aa4d37c12eecb001ef74a882f9e5c5be5fc9
Arron <UP2044509@myport.ac.uk>
2023-11-24 16:21:09 +0000

    feat: migrate to t3 app

--- site-content/src/pages/about.jsx
+++ site-content/src/pages/about.jsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
+
 export default function About() {
   return (
     <div className="flex min-h-screen min-w-screen">
