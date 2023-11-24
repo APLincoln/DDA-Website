c73e32d82dd29ee5b7e173d6d8e654770016a05b
Arron <UP2044509@myport.ac.uk>
2023-11-24 17:00:11 +0000

    feat: made new dynamic card for mobile and desktop

---
+++ dinas_drive/src/app/components/ImageCard/ImageCard.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import profilePic from "../../assets/profile-pic.png";
+import profilePic2 from "../../assets/red-profile-pic.png";
+import Image, { StaticImageData } from "next/image";
+import { Sarpanch } from "next/font/google";
+
+interface CardProps {
+  textContent: string[];
+  image: StaticImageData;
+  rounded: boolean;
+  reversed: boolean;
+}
+
+export default function Card({
+  textContent,
+  image,
+  rounded,
+  reversed = false,
+}: CardProps) {
+  return (
+    <>
+      <div
+        id="Persona card"
+        className={`flex h-full w-full flex-col items-center rounded-2xl bg-neutral-100 bg-opacity-50 lg:hidden lg:flex-row lg:px-2`}
+      >
+        <span className="flex h-52 w-52 items-center justify-center">
+          <Image
+            src={image}
+            alt="profile image"
+            height="180"
+            width="180"
+            className={`${rounded ? "rounded-full" : ""}`}
+          />
+        </span>
+        <div className="flex h-full w-full flex-col justify-center">
+          {textContent.map((text) => (
+            <p className="mt-1 px-4 text-center font-medium">{text}</p>
+          ))}
+        </div>
+      </div>
+      {reversed ? (
+        <div
+          id="Persona card"
+          className={`hidden h-full w-full flex-col items-center rounded-2xl bg-neutral-100 bg-opacity-50 lg:flex lg:flex-row lg:px-2`}
+        >
+          <span className="flex h-52 w-52 items-center justify-center">
+            <Image
+              src={image}
+              alt="profile image"
+              height="180"
+              width="180"
+              className={`${rounded ? "rounded-full" : ""}`}
+            />
+          </span>
+          <div className="flex h-full w-full flex-col justify-center">
+            {textContent.map((text) => (
+              <p className="mt-1 px-4 text-center font-medium">{text}</p>
+            ))}
+          </div>
+          <span className="w-52"></span>
+        </div>
+      ) : (
+        <div
+          id="Persona card"
+          className={`hidden h-full w-full flex-col items-center rounded-2xl bg-neutral-100 bg-opacity-50 lg:flex lg:flex-row lg:px-2`}
+        >
+          <span className="w-52"></span>
+          <div className="flex h-full w-full flex-col justify-center">
+            {textContent.map((text) => (
+              <p className="mt-1 px-4 text-center font-medium">{text}</p>
+            ))}
+          </div>
+          <span className="flex h-52 w-52 items-center justify-center">
+            <Image
+              src={image}
+              alt="profile image"
+              height="180"
+              width="180"
+              className={`${rounded ? "rounded-full" : ""}`}
+            />
+          </span>
+        </div>
+      )}
+    </>
+  );
+}
--- dinas_drive/src/app/layout.tsx
+++ dinas_drive/src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>
+      <body className={`font-sans ${inter.variable} min-h-screen`}>
         <Header />
         <TRPCReactProvider cookies={cookies().toString()}>
           {children}
